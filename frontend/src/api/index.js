6f86ca3563b475a5df7a93d8ceef5701a0ce4e8b
leong0103 <kwokheingleo@gmail.com>
2022-11-30 13:47:55 +0000

    Refactor

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -11,9 +11,6 @@ export const createAPIEndpoint = endpoint => {
     let url = BASE_URL + "api/" + endpoint + "/";
     return {
         fetch: () => axios.get(url),
-        fetchById: id => axios.get(url + id),
         post: newRecord => axios.post(url, newRecord),
-        put: (id, updatedRecord) => axios.put(url + id, updatedRecord),
-        delete: id => axios.delete(url + id),
     }
 }
